d0655533d80c09244ffebe5d05d21b571ef83057
Viktor Tyshchenko <vtitch@gmail.com>
2019-08-08 18:19:30 +0000

    init enigma directory

---
+++ secret_contracts/simple_addition/src/lib.rs
@@ -0,0 +1,37 @@
+// Rust’s standard library provides a lot of useful functionality, but assumes support for various 
+// features of its host system: threads, networking, heap allocation, and others. SGX environments
+// do not have these features, so we tell Rust that we don’t want to use the standard library
+#![no_std]
+
+// The eng_wasm crate allows to use the Enigma runtime, which provides:
+//     - Read from state      read_state!(key)        
+//     - Write to state       write_state!(key => value)
+//     - Print                eprint!(...)
+extern crate eng_wasm;
+
+// The eng_wasm_derive crate provides the following
+//     - Functions exposed by the contract that may be called from the Enigma network
+//     - Ability to call functions of ethereum contracts from ESC
+extern crate eng_wasm_derive;
+
+use eng_wasm::*;
+
+// For contract-exposed functions first include:
+use eng_wasm_derive::pub_interface;
+
+use eng_wasm::String;
+
+// For contract-exposed functions, declare such functions under the following public trait:
+#[pub_interface]
+pub trait ContractInterface{
+    fn addition(U256, U256) -> U256 ;
+}
+
+// The implementation of the exported ESC functions should be defined in the trait implementation 
+// for a new struct. 
+pub struct Contract;
+impl ContractInterface for Contract {
+    fn addition(x: U256, y: U256) -> U256 {
+        x + y
+    }
+}
