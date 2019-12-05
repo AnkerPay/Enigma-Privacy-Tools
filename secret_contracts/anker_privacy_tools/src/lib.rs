7bbcf4937155796025ff4c7266cfde5d13ce68cf
Viktor <vtitch@gmail.com>
2019-12-05 18:19:43 +0100

    Update lib.rs

--- secret_contracts/anker_privacy_tools/src/lib.rs
+++ secret_contracts/anker_privacy_tools/src/lib.rs
@@ -2,14 +2,11 @@
 #![no_std]
 
 // Imports
-extern crate eng_wasm;
-extern crate eng_wasm_derive;
-extern crate serde;
 use eng_wasm::*;
 use eng_wasm_derive::pub_interface;
 use serde::{Serialize, Deserialize};
 
-static PRIVACYINFO: &str = "PrivacyInfo";
+static PRIVACYINFO: &str = "PrivacyInfos";
 
 // Structs
 #[derive(Serialize, Deserialize)]
@@ -20,40 +17,30 @@ pub struct PrivacyInfo {
 
 
 // Public struct Contract which will consist of private and public-facing secret contract functions
-pub struct Contract;
+struct PrivacyContract;
 
-// Private functions accessible only by the secret contract
-impl Contract {
+
+#[pub_interface]
+
+impl PrivacyContract {
+
     fn get_emails() -> Vec<PrivacyInfo> {
         read_state!(PRIVACYINFO).unwrap_or_default()
     }
-}
 
-// Public trait defining public-facing secret contract functions
-#[pub_interface]
-pub trait ContractInterface{
-    fn add_email(email: String, ankaddress: String);
-    fn check_email(email: String) -> String;
-}
-
-// Implementation of the public-facing secret contract functions defined in the ContractInterface
-// trait implementation for the Contract struct above
-impl ContractInterface for Contract {
-    #[no_mangle]
-    fn add_email(email: String, ankaddress: String) {
-        let mut emails = Self::get_emails();
-        emails.push(PrivacyInfo {
+    pub fn add_email(email: String, ankaddress: String) {
+        let mut PrivacyInfos = Self::get_emails();
+        PrivacyInfos.push(PrivacyInfo {
             email,
             ankaddress,
         });
-        write_state!(PRIVACYINFO => emails);
+        write_state!(PRIVACYINFO => PrivacyInfos);
     }
 
-    #[no_mangle]
-    fn check_email(email: String) -> String {
-        let emails = Self::get_emails();
-        let index = emails.iter().position(|r| r.email == email).unwrap();
-        return emails[index].ankaddress.clone();
+    pub fn check_email(email: String) -> String {
+        let PrivacyInfos = Self::get_emails();
+        let index = PrivacyInfos.iter().position(|r| r.email == email).unwrap();
+        return PrivacyInfos[index].ankaddress.clone();
     }
 
 }
